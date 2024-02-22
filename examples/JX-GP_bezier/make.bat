f7d2da6e6a86686238f47ec75684ba153aaa3ec6
jxjo <58261241+jxjo@users.noreply.github.com>
2024-02-22 14:27:27 +0100

    update

--- examples/JX-GP_bezier/make.bat
+++ examples/JX-GP_bezier/make.bat
@@ -1,11 +1,11 @@
 @echo off
-set Airfoil=JX-GP_bez
+set Airfoil=JX-GP_bezier
 
 rem is xoptfoil2 in the parent directory? 
  
 set localPath=..\..\
 if not exist %localPath%xoptfoil2.exe set LocalPath=
 
-%LocalPath%xoptfoil2 -i %Airfoil%.inp -o %Airfoil%
+%LocalPath%xoptfoil2 -i %Airfoil%.inp -o %Airfoil%_opt
 
 pause
--- examples/JX-GP_hicks/make.bat
+++ examples/JX-GP_hicks/make.bat
@@ -1,13 +1,10 @@
 @echo off
-set Airfoil=JX-RS_bez
-
+set Airfoil=JX-GP_hicks
 rem is xoptfoil2 in the parent directory? 
  
 set localPath=..\..\
 if not exist %localPath%xoptfoil2.exe set LocalPath=
 
-%LocalPath%xoptfoil2 -i %Airfoil%.inp -o %Airfoil%
+%LocalPath%xoptfoil2 -i %Airfoil%.inp -o %Airfoil%_opt
 
 pause
-
-
