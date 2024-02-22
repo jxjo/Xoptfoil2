d3aefbde87d4421f919e80bdfde5a4089cb0f5c1
jxjo <58261241+jxjo@users.noreply.github.com>
2024-02-22 14:27:55 +0100

    update

--- examples/SD7003_camb_thick/make.bat
+++ examples/SD7003_camb_thick/make.bat
@@ -1,12 +1,12 @@
 @echo off
-set Airfoil=SD7003_opt
+set Airfoil=SD7003_cdmin
 
 rem is xoptfoil2 in the parent directory? 
  
 set localPath=..\..\
 if not exist %localPath%xoptfoil2.exe set LocalPath=
 
-%LocalPath%xoptfoil2 -i %Airfoil%.inp -o %Airfoil%
+%LocalPath%xoptfoil2 -i %Airfoil%.inp -o %Airfoil%_opt
 
 pause
 
