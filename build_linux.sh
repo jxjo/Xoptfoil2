0862c15d79a7f390dc095874dc81a85f5ac54e0f
jxjo <58261241+jxjo@users.noreply.github.com>
2024-03-03 17:45:44 +0100

    update Linux build - thanks Thomas!

--- build_linux.sh
+++ build_linux.sh
@@ -2,23 +2,23 @@
 
 BUILDDIR=build
 INSTALLDIR=$(pwd)/linux
-XOPTFOIL_VERSION=1.70beta
+CURRENT_DATE=`date +"%Y.%m.%d"`
+XOPTFOIL_VERSION=beta_${CURRENT_DATE} 
 TARGET_OS=UNIX
-
+ 
 export XOPTFOIL_VERSION
 export TARGET_OS
 
-if [ -d "$BUILDDIR" ]; then rm -Rf $BUILDDIR; fi
+if [ -d "$BUILDDIR" ];   then rm -Rf $BUILDDIR; fi
 if [ -d "$INSTALLDIR" ]; then rm -Rf $INSTALLDIR; fi
 
-mkdir build
-cd build
+mkdir $BUILDDIR
+cd $BUILDDIR
 
 cmake \
 -DCMAKE_INSTALL_PREFIX:PATH="$INSTALLDIR" \
 -DCMAKE_BUILD_TYPE:STRING="Release"  \
 ..
 make VERBOSE=1 || exit 1
-make install || exit 1
+make install   || exit 1
 
-cd ..
